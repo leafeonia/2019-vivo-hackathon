86f0e20b3a510a756ab414b568faa431403d6611
Cypher <Cypher0601@gmail.com>
2019-05-26 19:36:23 +0800

    all 5

--- android/app/src/main/java/advertisingspaceforrent/com/myapplication/MainSignUpActivity.java
+++ android/app/src/main/java/advertisingspaceforrent/com/myapplication/MainSignUpActivity.java
@@ -1,19 +1,31 @@
 package advertisingspaceforrent.com.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainSignUpActivity extends AppCompatActivity {
+
+    Button buttonbacktosignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_sign_up);
+        buttonbacktosignin = (Button)findViewById(R.id.signUp);
+        buttonbacktosignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainSignUpActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     //Used to show the help by triggering a toast
